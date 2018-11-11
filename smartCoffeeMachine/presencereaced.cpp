ee827d16c64d48eb004cf708732440e5f69288ee
Paolo Baldini <paolobaldini01@gmail.com>
2018-11-11 16:47:22 +0100

    hope hope hope hope...

--- smartCoffeeMachine/presencereaced.cpp
+++
@@ -1,9 +0,0 @@
-#include "global_var.h"
-#include "presencereader.h"
-#include "Arduino.h"
-
-bool pir_presence;
-
-void PresenceReader::Exec() {
-  pir_presence = digitalRead( PIR_PIN ) == HIGH;
-}
